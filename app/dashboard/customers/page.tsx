0aff3c577be928e1374bb8162ef5763797c9ec12
Anshul <100751856+hi-anshul@users.noreply.github.com>
2024-02-02 18:00:48 +0530

    Uploading Dashboard
    Uploading Dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Customers = () => {
+  return (
+    <div>This is Customers Page</div>
+  )
+}
+
+export default Customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Invoices = () => {
+  return (
+    <div>This is Invoices Page</div>
+  )
+}
+
+export default Invoices
